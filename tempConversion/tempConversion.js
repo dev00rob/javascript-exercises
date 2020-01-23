01fc1f42c89de4aecda196b1493cb53af503151f
dev rob <dev00rob@gmail.com>
2020-01-23 15:26:19 -0800

    Solve tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  // (F - 32) / 1.8 = C
+  return Number(((f - 32) / 1.8).toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+  // (C * 1.8) + 32 = F
+  return Number(((c * 1.8) + 32).toFixed(1));
 }
 
 module.exports = {
