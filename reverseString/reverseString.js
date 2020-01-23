e88d919472933f6b9146f975cf724ac1f77103ea
dev rob <dev00rob@gmail.com>
2020-01-23 10:18:07 -0800

    Solve reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = "";
+    for (let i = str.length - 1; i > -1; i--){
+        result += str.charAt(i);
+    }
 
+    return result;
 }
 
 module.exports = reverseString
