1ccb2625d6c96acc71a5f834c6cf42f26d624f0a
dev rob <dev00rob@gmail.com>
2020-01-23 14:34:51 -0800

    Solve removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let inputArr = arguments[0];
 
+    for (let i = 1; i < arguments.length; i++){
+        for (let j = 0; j < inputArr.length; j++){
+            if (inputArr[j] === arguments[i]){
+                inputArr.splice(j,1);
+            }
+        }
+    }
+
+    return inputArr;
 }
 
 module.exports = removeFromArray
