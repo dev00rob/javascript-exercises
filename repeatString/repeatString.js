56074f98885b662e2dc187c79130183821fd2f25
dev rob <dev00rob@gmail.com>
2020-01-23 10:06:17 -0800

    Solve repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str,repeats) {
+    if (repeats < 0){ return "ERROR"; }
 
+    let result = "";
+    while (repeats--){ result += str; }
+
+    return result;
 }
 
 module.exports = repeatString
